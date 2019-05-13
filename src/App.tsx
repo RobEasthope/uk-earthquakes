c872c743192487715bf21b5dea9bc1ede828aff0
Rob Easthope <hello@robeasthope.com>
2019-05-13 09:42:32 +0100

    Test react hooks code

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,35 @@
 import React from 'react';
 import Map from './Map';
 
+function User() {
+  const [firstName, setFirstName] = React.useState(null);
+  const [lastName, setLastName] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch('https://randomuser.me/api/')
+      .then(results => results.json())
+      .then(data => {
+        const { name } = data.results[0];
+        setFirstName(name.first);
+        setLastName(name.last);
+      });
+  }, []); // <-- Have to pass in [] here!
+
+  return (
+    <div>
+      Name:{' '}
+      {!firstName || !lastName ? 'Loading...' : `${firstName} ${lastName}`}
+    </div>
+  );
+}
+
 const App: React.FC = () => {
-  return <Map />;
+  return (
+    <React.Fragment>
+      <User />
+      {/* <Map /> */}
+    </React.Fragment>
+  );
 };
 
 export default App;
